a186ef85e2c49288cd1fb06f0a910419214daaab
Porter McGary <portermcgary@gmail.com>
2021-03-17 21:21:00 -0600

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Porter McGary on 3/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Programming_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_CalculatorApp.swift
+//  Shared
+//
+//  Created by Porter McGary on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
